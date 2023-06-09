2a1f5129ea855eb190838187c86837757677f2c8
Khaled Abdulaziz <khaled.ibrahim.user@gmail.com>
2023-06-10 01:18:59 +0300

    Add more organized comments
    Add header for the script including file name and description
    Improve the visuals of the comments to be above lines of codes

--- 02_LinuxAdmin/Bash_Scripting/Labs/lab1.sh
+++ 02_LinuxAdmin/Bash_Scripting/Labs/lab1.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
+# Script: lab1.sh
+# Description: This script prompts the user to enter their personal information and stores it in a file.
 
-read -p "Whats Your Name?: " USER_NAME  # Prompt the user to enter their name
+# Prompt the user to enter their name
+read -p "What's Your Name?: " USER_NAME
 echo "Hello $USER_NAME"  # Greet the user with their name
-echo "I will create a file called ${USER_NAME}_file"  # Notify the user about the file creation
-touch "${USER_NAME}_file"  # Create a file with the user's name
 
-echo "Name: $USER_NAME" >> "${USER_NAME}_file"  # Append the user's name to the file
+# Notify the user about the file creation
+echo "I will create a file called ${USER_NAME}_file"
 
-read -p "Enter your university: " UNIVERSITY  # Prompt the user to enter their university
+# Create a file with the user's name
+touch "${USER_NAME}_file"
+
+# Append the user's name to the file
+echo "Name: $USER_NAME" >> "${USER_NAME}_file"
+
+# Prompt the user to enter their university
+read -p "Enter your university: " UNIVERSITY
 echo "University: $UNIVERSITY" >> "${USER_NAME}_file"  # Append the university to the file
 
-read -p "Enter your age: " AGE  # Prompt the user to enter their age
+# Prompt the user to enter their age
+read -p "Enter your age: " AGE
 echo "Age: $AGE" >> "${USER_NAME}_file"  # Append the age to the file
 
-read -p "Where are you from?: " LOCATION  # Prompt the user to enter their location
+# Prompt the user to enter their location
+read -p "Where are you from?: " LOCATION
 echo "Location: $LOCATION" >> "${USER_NAME}_file"  # Append the location to the file
 
-echo "Data stored in ${USER_NAME}_file."  # Notify the user about the successful storage of data in the file.
+# Notify the user about the successful storage of data in the file
+echo "Data stored in ${USER_NAME}_file."
